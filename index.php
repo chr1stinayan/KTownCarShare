2014b9e1abc8d4be7fd776162c7e088b32124187
christinayan <christina.yan@queensu.ca>
2017-03-25 17:13:22 -0400

    Updated KTCS landing page

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 
     <link rel="shortcut icon" href="img/favicon.png">
 
-    <title>QThrive - Better mental health through journaling </title>
+    <title>K-Town Car Share - Where will your next trip take you? </title>
 
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.css" rel="stylesheet">
@@ -21,9 +21,12 @@
 
     <!-- Fonts from Google Fonts -->
 	<link href='http://fonts.googleapis.com/css?family=Lato:300,400,900' rel='stylesheet' type='text/css'>
+	<link href="https://fonts.googleapis.com/css?family=Slabo+27px" rel="stylesheet">
+	
+  
   <style>
   #headerwrap{
-    background-image: url("img/time2.jpg");
+    background-image: url("img/lake.jpg");
     background-size: cover;
     -webkit-background-size: cover;
     -moz-background-size: cover;
@@ -66,7 +69,7 @@
 
 		        </button>
 
-	         	<a class="navbar-brand" href="index.php"> <img src="img/QThrive.png" height="50"> <b>QThrive</b> </a>
+	         	<a class="navbar-brand" href="index.php"> <img src="img/logo v1.png" height="50"> <b>K-Town Car Share</b> </a>
 
 	        </div>
 
@@ -93,7 +96,7 @@
 
 				<div class="col-lg-6 col-lg-offset-6">
 
-					<h1>Work towards better mental health... one journal entry at a time.</h1>
+					<h1>Quick, convenient car share service.</h1>
 
 					<form action="signup.php" method="GET" class="form-inline" role="form">
 
@@ -130,7 +133,7 @@
 
 			<div class="col-lg-6 col-lg-offset-3">
 
-				<h1>Pain is real, but so is hope.</h1>
+				<h1>Car sharing made simple.</h1>
 
 			</div>
 
@@ -141,33 +144,33 @@
 
 			<div class="col-lg-4">
 
-				<a href="chat.php"><img src="img/index01.png" height="180" alt=""></a>
+				<a href="chat.php"><img src="img/car.png" height="180" alt=""></a>
 
-				<h4>Chat.</h4>
+				<h4>From economy to luxury.</h4>
 
-				<p>Need someone to talk to?<br>Meet Sona.<br>Sona is a chatbot that helps facilitate journaling.</p>
-
-			</div>
-			<!--/col-lg-4 -->
-
-			<div class="col-lg-4">
-
-				<a href="journal.php"><img src="img/index02.png" height="180" alt=""></a>
-
-				<h4>Journal.</h4>
-
-				<p>Stressed? Upset? Need to vent?<br>Take a minute to record your thoughts.<br>We're here for you, always.</p>
+				<p>Choose from a wide selection of vehicles.<br>For any occasion or trip, KTCS has the car for you.</p>
 
 			</div>
 			<!--/col-lg-4 -->
 
 			<div class="col-lg-4">
 
-				<a href="jentries.php"><img src="img/index03.png" height="180" alt=""></a>
+				<a href="journal.php"><img src="img/booking.png" height="180" alt=""></a>
 
-				<h4>Thrive.</h4>
+				<h4>Easy reservations.</h4>
 
-				<p>Access local resources.<br>Look at personal growth.<br>Be well.</p>
+				<p>Instant online booking system.<br>Pickup your car within minutes of booking.</p>
+
+			</div>
+			<!--/col-lg-4 -->
+
+			<div class="col-lg-4">
+
+				<a href="jentries.php"><img src="img/parking.png" height="180" alt=""></a>
+
+				<h4>Pickup Locations.</h4>
+
+				<p>Lots near Queen's, RMC and SLC.<br>Pickup and drop off your car at any lot of your choice.</p>
 
 			</div>
 			<!--/col-lg-4 -->
@@ -188,9 +191,9 @@
 
                 <div class="col-lg-6 col-lg-offset-3">
 
-                    <h1>QThrive is for students.</h1>
+                    <h1>K-Town Car Share starts local.</h1>
 
-                    <h3>Take the challenges and joys of life one day at a time.</h3>
+                    <h3>Where will your next trip take you?</h3>
 
                 </div>
 
@@ -222,7 +225,7 @@
 
 	<div class="container">
 
-		<p class="centered">Created by Ben, Sean, Vinyas &amp; Vinith for QHacks 2017.</p>
+		<p class="centered">CISC 332 Final Project created by Christina Yan &amp; Vinith Suriyakumar.</p>
 
 	</div>
 	<!-- /container -->

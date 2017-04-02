8d0cab94d2a8f7d58365d88d0e72bdba1369dd34
christinayan <christina.yan@queensu.ca>
2017-04-02 12:03:09 -0400

    Time in HH:mm:ss format now.
    Datepicker range broke again

--- newBooking.php
+++ newBooking.php
@@ -1,59 +1,81 @@
+<?php 
+  if (!isset($_POST['sign_up'])){ 
+    if(isset($_SESSION['Member_ID'])) { 
+      session_start(); 
+      header("Location: profile.php"); 
+      die(); 
+    } 
+  } 
+  else { 
+      include_once 'config/connection.php'; 
+        $query = "SELECT Email FROM Member WHERE Email=?"; 
+        if($stmt = $con->prepare($query)) { 
+          $stmt->bind_Param("s", $_POST['Email']); 
+      $stmt->execute(); 
+      $result = $stmt->get_result(); 
+      $num = $result->num_rows; 
+      if($num===0) { 
+            $query = "INSERT INTO Member (F_Name,L_Name,Email,Phone_No,Grad_Year,Faculty,Degree_Type,Password) 
+            VALUES ('$_POST[FirstName]','$_POST[LastName]','$_POST[Email]','$_POST[Phone]','$_POST[Year]','$_POST[Faculty]','$_POST[Degree]','$_POST[Password]')"; 
+            mysqli_query($con,$query); 
+            $query = "SELECT Member_ID FROM Member WHERE Email = '$_POST[Email]'"; 
+            $myrow = mysqli_query($con,$query)->fetch_assoc(); 
+        $_SESSION['Member_ID'] = $myrow['Member_ID']; 
+        header("Location:chat.php"); 
+        exit(); 
+      } else { 
+        error_log("Email already in use"); 
+        header("Location:signup.php?msg=bademail"); 
+      } 
+    } else { 
+      echo "failed to prepare the SQL"; 
+    } 
+  } 
+?> 
+
 <!DOCTYPE html>
 var date = ""
 <html lang="en">
-		<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta name="description" content="">
-		<meta name="author" content="VMS&CY">
-		<link rel="shortcut icon" href="img/logo v1.jpg">
-		
-		<title>K-Town Car Share - Landing Page</title>
-		<link href="css/bootstrap.css" rel="stylesheet">
-		<link href="css/main.css" rel="stylesheet">
-		<link href="css/bootstrap-datepicker.css", rel="stylesheet">		
-		<link href='http://fonts.googleapis.com/css?family=Lato:300,400,900' rel='stylesheet' type='text/css'>
-		<link rel="stylesheet" type="text/css"
-        href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" />
-		<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-		<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-        <script src="//cdnjs.cloudflare.com/ajax/libs/timepicker/1.3.5/jquery.timepicker.min.js"></script>
-		<script src="js/bootstrap-datepicker.js"></script>
-		<script type="text/javascript">
-		   $(function(){
-				$("#pDate").datepicker({
-					minDate:0,
-					onSelect: function(date){
-						$("#rDate").datepicker('option','minDate',date);
-					}
-				});
-		   });
-		</script>
-	    <script type="text/javascript">
-		   $(function(){
-				$("#rDate").datepicker({});
-		   });
-        </script>
-		<script type="text/javascript">
-		   $(function(){
-				$("#pTime").timepicker();
-		   });
-        </script>
-		<script type="text/javascript">
-		   $(function(){
-				$("#rTime").timepicker();
-		   });
-        </script>
-		<script>
-			$(function myFunction() {
-				var no = document.getElementById("Lot List");
-				var lot = no.options[no.selectedIndex].text;
-				document.getElementById("lotSel").innerHTML = lot;
-				
-								
-			});
-		</script>
+	<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="">
+	<meta name="author" content="VMS&CY">
+	<link rel="shortcut icon" href="img/logo v1.jpg">
 	
+	<title>K-Town Car Share - Landing Page</title>
+	<link href="css/bootstrap.css" rel="stylesheet">
+	<link href="css/main.css" rel="stylesheet">
+	<link href="css/bootstrap-datepicker.css", rel="stylesheet">		
+	<link href='http://fonts.googleapis.com/css?family=Lato:300,400,900' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css"
+	href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" />
+	<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/timepicker/1.3.5/jquery.timepicker.min.js"></script>
+	<script src="js/bootstrap-datepicker.js"></script>
+	<script type="text/javascript">
+		$(function(){ 
+			$("#pDate").datepicker({ 
+				minDate:0, 
+				onSelect: function(date){ 
+				$("#rDate").datepicker('option','minDate',date); 
+				} 
+			}); 
+		}); 
+    </script> 
+    <script type="text/javascript"> 
+		$(function(){ 
+			$("#rDate").datepicker({}); 
+       }); 
+    </script>
+	<script type="text/javascript">
+	   $(function(){
+			$("#pTime").timepicker({ 'timeFormat': 'HH:mm:ss' });
+			$("#rTime").timepicker({ 'timeFormat': 'HH:mm:ss' });
+	   });
+	</script>
+
     </head>
 	<body>
 		<div class="navbar navbar-default navbar-fixed-top">
@@ -160,7 +182,7 @@ var date = ""
 								<button type="button" class="close" data-dismiss="modal">&times;</button>
 								<h4 class="modal-title">Confirm Booking Details</h4>
 							</div>
-							<div class="modal-body" onload="myFunction()">
+							<div class="modal-body">
 								<div class="row">
 									<p style="color: #041530">Car: </p>
 									<p id="carSel" style="color: #041530"></p>

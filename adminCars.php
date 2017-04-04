92037412dee75387e612a09f41dfae66005fbfe2
Vinith MS <suriyaku@gmail.com>
2017-04-04 00:04:24 -0400

    Admin pages changes

---
+++ adminCars.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="">
+			<meta name="author" content="VMS&amp;SN&amp;BB&amp;VH">
+		<link rel="shortcut icon" href="img/logo v1.jpg">
+		<title>K-Town Car Share - Cars</title>
+		<link href="css/bootstrap.css" rel="stylesheet">
+		<link href="css/main.css" rel="stylesheet">
+		<link href='http://fonts.googleapis.com/css?family=Lato:300,400,900' rel='stylesheet' type='text/css'>
+			<link rel="stylesheet" type="text/css"
+		href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css">
+		<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+		<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/timepicker/1.3.5/jquery.timepicker.min.js"></script>
+		<script src="js/bootstrap-datepicker.js"></script>
+		<script type="text/javascript">
+		$(function(){ 
+			$("#pickUpDate").datepicker({ 
+				minDate:0, 
+				onSelect: function(date){ 
+				$("#dropOffDate").datepicker('option','minDate',date); 
+				} 
+			}); 
+		}); 
+    </script> 
+    <script type="text/javascript"> 
+		$(function(){ 
+			$("#dropOffDate").datepicker({}); 
+       }); 
+    </script>
+	<script type="text/javascript">
+	   $(function(){
+			$("#pickUpTime").timepicker({ 'timeFormat': 'HH:mm:ss' });
+			$("#dropOffTime").timepicker({ 'timeFormat': 'HH:mm:ss' });
+	   });
+	</script>
+
+	</head>
+	<body>
+	<?php
+		session_start();
+        if (isset($_POST['addCar'])){
+            $query = "INSERT INTO CAR (make,model,year,lotAddress,dailyFee,carCondition,picturePath,carOdometer) VALUES(?,?,?,?,?,'normal',?,0.00)";
+            if($stmt = $con->prepare($query)){
+                $stmt->bind_Param("ssisds",$_POST['make'],$_POST['model'],$_POST['year'],$_POST['lotAddress'],$_POST['dailyFee'],$_POST['ImagePath']);
+                $stmt->execute();
+            }
+        }
+	?>
+			<div class="navbar navbar-default navbar-fixed-top">
+				<div class="container">
+					<div class="navbar-header">
+							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+						<a class="navbar-brand" href="index.php"> <img src="img/logo v1.png" height="50"> <b>K-Town Car Share</b> </a>
+					</div>
+					<div class="navbar-collapse collapse">
+					<ul class="nav navbar-nav navbasr-right" style="padding-top: 15px;">
+						<li><a href="cars.php">Cars</a></li>
+						<li><a href="lots.php">Pickup & Dropoff Lots</a></li>
+						<li><a href="currentRes.php">My Reservations</a></li>
+						<li><a href="newRes.php">New Booking</a></li>
+						<li><a href="editprofile.php">My Profile</a></li>
+						<li><a href="login.php?logout=1">Sign Out</a></li>						
+					</ul>
+					</div>
+				</div>
+			</div>
+		<div id="headerwrap" style="padding-top: 50px; min-height: 590px;">
+			<div class="container">
+				<div class="form-group">
+					<div class="col-lg-9-offset-1" style="padding-bottom:15px">
+						<h1 style="text-align: center; color: #041530;"></h1>
+				</div>
+				<div class="row">
+					<div class="col-lg-4 col-lg-offset-4">
+						<h1 style="text-align:center">Search For Cars</h1>
+						<form name='addCar' id='addCar' action='adminCars.php' method='POST'>
+						 	<div class="form-group">
+						    	<input style="width: 100%;" name="Make" type="text" class="form-control" id="Make" placeholder="Make">
+						  	</div>
+						 	<div class="form-group">
+						    	<input style="width: 100%;" name="Model" type="text" class="form-control" id="Model" placeholder="Model">
+						 	</div>
+							 <div class="form-group">
+						    	<input style="width: 100%;" name="Year" type="text" class="form-control" id="Year" placeholder="Year">
+						 	</div>
+							 <div class="form-group">
+						    	<input style="width: 100%;" name="DailyFee" type="text" class="form-control" id="DailyFee" placeholder="Daily Fee">
+						 	</div>
+							<div class="form-group">
+								<select style="width: 100%;" type="text" class="form-control" name="pickUpLocation" id="pickUpLocation">
+								<option value="" selected disabled>Pickup Location </option>
+								<?php
+									include 'config/connection.php';
+									$locationQueryResult = "SELECT * FROM ParkingLocations";
+									$locationQueryResult = mysqli_query($con,$locationQueryResult);
+									if(!locationQueryResult){
+										printf("Error: %s\n", mysqli_error($con));
+   										exit();
+									}
+									while($location = mysqli_fetch_array($locationQueryResult)){
+										echo "<option value=''>".$location['lotAddress']."</option>";
+									}
+								?>
+								</select>
+							</div>
+                             <div class="form-group">
+						    	<input style="width: 100%;" name="ImagePath" type="text" class="form-control" id="ImagePath" placeholder="Upload a picture">
+						 	</div>
+						 	<div style="text-align: center">
+								<button type="submit" class="btn btn-warning btn-lg", name="addCar">Add Car</button>
+							</div>
+						</form>
+					</div>
+				</div>
+                <div class="form-group">
+                    <h1> Show Rental History For Car </h1>
+				</div>
+                <form name='chosenCar' id='chosenCar' action='adminCars.php' method='POST'>
+                <div class="form-group">
+								<select style="width: 100%;" type="text" class="form-control" name="chosenVIN" id="chosenVIN">
+								<option value="" selected disabled>Choose Car</option>
+								<?php
+									include 'config/connection.php';
+									$carQueryResult = "SELECT * FROM CAR";
+									$carQueryResult = mysqli_query($con,$carQueryResult);
+									if(!carQueryResult){
+										printf("Error: %s\n", mysqli_error($con));
+   										exit();
+									}
+									while($car = mysqli_fetch_array($carQueryResult)){
+										echo "<option value='".$car['VIN']."'>".$car['year']." ".$car['make']." ".$car['model']."</option>";
+									}
+								?>
+								</select>
+				</div>
+                <div style="text-align: center">
+						<button type="submit" class="btn btn-warning btn-lg", name="chosenCar">Add Car</button>
+				</div>
+                </form>
+                <?php 
+                    if(isset($_POST['chosenVIN'])){
+                        include 'config/connection.php';
+                        $rentalHistoryQueryResult = "SELECT DISTINCT DATEDIFF(DATE(dropOffDateTime), DATE(pickUpDateTime)) as length, (dropOffOdometer-pickUpOdometer) as distance, lotAddress, pickUpState, dropOffState, VIN FROM (RentalHistory NATURAL JOIN Reservation) WHERE VIN=".$_POST['chosenVIN']." AND startDateTime < '".date('y:m:d h:i:s')."'";
+                        $rentalHistoryQueryResult = mysqli_query($con,$rentalHistoryQueryResult);
+                        if(!$rentalHistoryQueryResult){
+                            printf("Error: %s\n", mysqli_error($con));
+   							exit();
+                        }
+                        while($rentalHistory=mysqli_fetch_array($rentalHistoryQueryResult)){
+                            $length = $rentalHistory['length'];
+										$lot = $rentalHistory['lotAddress'];
+										$distance = $rentalHistory['distance'];
+										$pickupState = $rentalHistory['pickUpState'];
+										$returnState = $rentalHistory['dropOffState'];
+										
+										echo "<tr>
+											<td>".$length." days </td>
+											<td>".$lot."</td>
+											<td>".$distance." km </td>
+											<td>".$pickupState."</td>
+											<td>".$returnState."</td>
+										    </tr>";
+                        }
+                    }
+                ?>
+			</div>
+		</div>
+		<div class="container">
+			<hr>
+			<p class="centered">CISC 332 Final Project created by Christina Yan &amp; Vinith Suriyakumar.</p>
+		</div>
+		<script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+			<script src="js/bootstrap.min.js"></script>
+		</body>
+</html>
--- adminLanding.php
+++ adminLanding.php
@@ -21,6 +21,13 @@ var date = ""
 	
 	<html>
 		<body> 
+		<?php
+			session_start();
+			if($_SESSION['userType']=='client'){
+            header('Location: profile.php');
+            exit();
+        }
+		?>
 			<div class="navbar navbar-default navbar-fixed-top"> 
 				<div class="container"> 
 					<div class="navbar-header"> 
@@ -48,9 +55,79 @@ var date = ""
 				<div class="container"> 
 					<div class="form-group">
 						<div class="col-lg-6 col-lg-offset-0">
-							<h1 style="color: #041530"><u>Today's reservations<br></u><h1>
-							<h2 style="color: #041530"><u>Picking up today<br></u><h2>
-							<h2 style="color: #041530"><u>Returning today<br></u><h2>
+							<h1 style="color: #041530"><u>Today's reservations<br></u></h1>
+							<?php
+								include 'config/connection.php';
+								$today = date("Y-m-d H:i:s");
+								$todayReservationResult = "SELECT * FROM Reservation WHERE startDateTime='".$today."'";
+								$todayReservationResult = mysqli_query($con,$todayReservationResult);
+								if(!$todayReservationResult){
+									printf("Error: %s\n", mysqli_error($con));
+   									exit();
+								}
+								while($reservation = mysqli_fetch_array($todayReservationResult)){
+									echo "<h2> Reservation Number:".$reservation['reservationNumber']."</h2>";
+								}
+							?>
+							<h2 style="color: #041530"><u>Returning today<br></u></h2>
+							<?php
+								include 'config/connection.php';
+								$today = date("Y-m-d H:i:s");
+								$todayReservationResult = "SELECT * FROM Reservation WHERE endDateTime='".$today."'";
+								$todayReservationResult = mysqli_query($con,$todayReservationResult);
+								if(!$todayReservationResult){
+									printf("Error: %s\n", mysqli_error($con));
+   									exit();
+								}
+								while($reservation = mysqli_fetch_array($todayReservationResult)){
+									echo "<h2> Reservation Number:".$reservation['reservationNumber']."</h2>";
+								}
+							?>
+							<form name='5000club' id='5000club' action='adminLanding.php' method='POST'>
+						 	<div style="text-align: center">
+								<button type="submit" class="btn btn-warning btn-lg", name="5000club">More than 5000 km</button>
+							</div>
+							</form>
+							<?php
+								include 'config/connection.php';
+								if(isset($_POST['5000club'])){
+									$today = date("Y-m-d H:i:s");
+									$todayReservationResult = "SELECT * FROM Reservation WHERE endDateTime='".$today."'";
+									$todayReservationResult = mysqli_query($con,$todayReservationResult);
+									if(!$todayReservationResult){
+										printf("Error: %s\n", mysqli_error($con));
+   										exit();
+									}
+									while($reservation = mysqli_fetch_array($todayReservationResult)){
+										echo "<h2> Reservation Number:".$reservation['reservationNumber']."</h2>";
+									}
+								}
+							?>
+							<form name='highestRentedCar' id='highestRentedCar' action='adminLanding.php' method='POST'>
+						 	<div style="text-align: center">
+								<button type="submit" class="btn btn-warning btn-lg", name="highestRentedCar">Highest Rented Car</button>
+							</div>
+							</form>
+							<?php
+								include 'config/connection.php';
+								if(isset($_POST['highestRentedCar'])){
+									$today = date("Y-m-d H:i:s");
+									$highestCarReservationResult = "SELECT VIN,max(rentCount) FROM (SELECT VIN,COUNT(*) as rentCount FROM RentalHistory GROUP BY VIN) as t1";
+									$highestReservationResult = mysqli_query($con,$highestCarReservationResult);
+									if(!$highestCarReservationResult){
+										printf("Error: %s\n", mysqli_error($con));
+   										exit();
+									}
+									while($reservation = mysqli_fetch_array($highestReservationResult)){
+										echo "<h2> Reservation Number:".$reservation['reservationNumber']."</h2>";
+									}
+								}
+							?>
+							<form name='lowestRentedCar' id='highestRentedCar' action='adminLanding.php' method='POST'>
+						 	<div style="text-align: center">
+								<button type="submit" class="btn btn-warning btn-lg", name="highestRentedCar">Highest Rented Car</button>
+							</div>
+							</form>
 						</div>
 					</div>					
 				</div>				
